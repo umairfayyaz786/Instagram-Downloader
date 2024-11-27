06b0294d79eb4a1499b19ef6883de57aaaa568f3
umairfayyaz786 <umairfayyaz12345@gmail.com>
2024-11-28 01:50:54 +0500

    updated

--- app/src/main/java/com/instagram/downloader/Fragments/AboutFragment.java
+++ app/src/main/java/com/instagram/downloader/Fragments/AboutFragment.java
@@ -42,31 +42,31 @@ public class AboutFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-//        binding.linearLayoutShare.setOnClickListener(v -> {
-//            Intent shareIntent = new Intent(Intent.ACTION_SEND);
-//            shareIntent.setType("text/plain");
-//            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "My application name");
-//            String shareMessage = "In Insta \n\n Hello Let me Recommend you this App Source Code @Github\n Download easy Instagram Story's, Post's, Reels IGTV and More. ";
-//            shareMessage = shareMessage + "https://github.com/AndroidWithRossyn/Instagram-Downloader";
-//            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
-//            startActivity(Intent.createChooser(shareIntent, "choose one"));
-//
-//        });
+        binding.share.setOnClickListener(v -> {
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, "My application name");
+            String shareMessage = "Instagram Downloader \n";
+            shareMessage = shareMessage + "https://github.com/umairfayyaz786/Instagram-Downloader.git";
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
+            startActivity(Intent.createChooser(shareIntent, "choose one"));
 
-//        binding.linearLayoutRate.setOnClickListener(v -> new RateDialog(requireActivity(), false).show());
+        });
 
-//        binding.linearLayoutFeedBack.setOnClickListener(v -> {
-//
-//            Intent intent = new Intent(Intent.ACTION_SENDTO);
-//            intent.setData(Uri.parse("mailto:" + requireActivity().getString(R.string.email_feedback)));
-//            intent.setPackage("com.google.android.gm");
-//            intent.putExtra(Intent.EXTRA_SUBJECT, requireActivity().getString(R.string.app_name));
-//            startActivity(intent);
-//
-//
-//        });
+        binding.rate.setOnClickListener(v -> new RateDialog(requireActivity(), false).show());
 
-//        binding.linearLayoutPrivacy.setOnClickListener(v -> startActivity(new Intent("android.intent.action.VIEW", Uri.parse(getString(R.string.policy_url)))));
+        binding.feedback.setOnClickListener(v -> {
+
+            Intent intent = new Intent(Intent.ACTION_SENDTO);
+            intent.setData(Uri.parse("mailto:" + requireActivity().getString(R.string.email_feedback)));
+            intent.setPackage("com.google.android.gm");
+            intent.putExtra(Intent.EXTRA_SUBJECT, requireActivity().getString(R.string.app_name));
+            startActivity(intent);
+
+
+        });
+
+        binding.privacyPolicy.setOnClickListener(v -> startActivity(new Intent("android.intent.action.VIEW", Uri.parse(getString(R.string.policy_url)))));
 //
 //        binding.linearLayoutApps.setOnClickListener(v -> startActivity(new Intent("android.intent.action.VIEW", Uri.parse(getString(R.string.developer_account_link)))));
 //
